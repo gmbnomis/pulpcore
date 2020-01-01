00f284b2aa00cfadad5347e7d2ec33df10cf05a1
Simon Baatz <gmbnomis@gmail.com>
2020-01-01 17:32:00 +0100

    Avoid superfluous RepositoryContent records
    When adding and removing the same content in a repository version, don't
    update the RepositoryContent record (resulting in an superfluous
    RepositoryContent for which version_added == version_removed == current
    version).  Instead, just delete the existing record.  This fixes two
    problems:
    
    1. Such content is shown as added and removed.
    2. Re-adding the content again in the same version leads to a
       IntegrityError because the uniqueness constraint is violated.
    
    Handle the "remove then re-add" case analogously.
    
    https://pulp.plan.io/issues/5707
    closes #5707

--- pulpcore/tests/unit/models/test_repository.py
+++ pulpcore/tests/unit/models/test_repository.py
@@ -12,16 +12,17 @@ class RepositoryVersionTestCase(TestCase):
         self.repository.save()
 
         contents = []
-        for _ in range(0, 4):
+        for _ in range(0, 20):
             contents.append(Content(pulp_type="core.content"))
 
         Content.objects.bulk_create(contents)
         self.pks = [c.pk for c in contents]
+        self.one_content_qs = [Content.objects.filter(pk=pk) for pk in self.pks]
 
     def test_add_and_remove_content(self):
-        contents = Content.objects.filter(pk__in=self.pks)
+        contents = Content.objects.filter(pk__in=self.pks[:4])
         with self.repository.new_version() as version1:
-            version1.add_content(contents)  # v1 == all contents
+            version1.add_content(contents)  # v1 == four content units
 
         to_remove = contents[0:2]
         with self.repository.new_version() as version2:
@@ -63,3 +64,54 @@ class RepositoryVersionTestCase(TestCase):
 
         self.assertCountEqual(added_pks_2, compress(self.pks, [1, 0, 0, 0]), added_pks_2)
         self.assertCountEqual(removed_pks_2, compress(self.pks, [0, 0, 0, 0]), removed_pks_2)
+
+    def content_qs(self, pks):
+        return Content.objects.filter(pk__in=pks)
+
+    def test_regularize_content(self):
+        with self.repository.new_version() as version1:
+            version1.add_content(self.content_qs(self.pks[:5]))  # v1 == content 0-4
+
+        # v2 content:
+        # 0 leave as is
+        # 1 remove
+        # 2 remove, re-add
+        # 3 remove, re-add, remove
+        # 4 remove, re-add, remove, re-add
+        # 5 add
+        # 6 add, remove
+        # 7 add, remove, re-add
+        # 8 add, remone, re-add, remove
+        # Expected content: 0, 2, 4, 5, 7
+        # Added: 5, 7
+        # Removed: 1, 3
+        with self.repository.new_version() as version2:  # v2 == content 0, 1 and 3
+            version2.remove_content(self.content_qs(self.pks[1:5]))
+            version2.add_content(self.content_qs(self.pks[2:5]))
+            version2.remove_content(self.content_qs(self.pks[3:5]))
+            version2.add_content(self.content_qs(self.pks[4:5]))
+
+            version2.add_content(self.content_qs(self.pks[5:9]))
+            version2.remove_content(self.content_qs(self.pks[6:9]))
+            version2.add_content(self.content_qs(self.pks[7:9]))
+            version2.remove_content(self.content_qs(self.pks[8:9]))
+
+        content_pks_1 = version1.content.values_list('pk', flat=True)
+        added_pks_1 = version1.added().values_list('pk', flat=True)
+        removed_pks_1 = version1.removed().values_list('pk', flat=True)
+
+        self.assertCountEqual(content_pks_1, self.pks[0:5], content_pks_1)
+        self.assertCountEqual(removed_pks_1, [], removed_pks_1)
+        self.assertCountEqual(added_pks_1, self.pks[0:5], added_pks_1)
+
+        content_pks_2 = version2.content.values_list('pk', flat=True)
+        added_pks_2 = version2.added().values_list('pk', flat=True)
+        removed_pks_2 = version2.removed().values_list('pk', flat=True)
+
+        self.assertCountEqual(
+            content_pks_2, compress(self.pks, [1, 0, 1, 0, 1, 1, 0, 1]), content_pks_1
+        )
+        self.assertCountEqual(removed_pks_2, compress(self.pks, [0, 1, 0, 1]), removed_pks_2)
+        self.assertCountEqual(
+            added_pks_2, compress(self.pks, [0, 0, 0, 0, 0, 1, 0, 1]), added_pks_2
+        )
